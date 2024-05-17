0eca2fc3a1dea62f434132b213df8f33aff4dbfc
Shafaqsardar2 <shafaqmateen949@gmail.com>
2024-05-17 13:39:24 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let guess = 5;
+
+let guessAns = await inquirer.prompt({
+    name : "your guess",
+    message : "Guess the number between 1 to 10",
+    type: "number"
+})
+if (guessAns["your guess"] === guess){
+    console.log("Congratulations! your guess is correct")
+} else {
+    console.log ("Oops! your guess is wrong")
+}
